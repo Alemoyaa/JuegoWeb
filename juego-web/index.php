8081a86cb4a4795d8971e43723a0b652542b89a2
Alejandro Moya <47389285+Alemoyaa@users.noreply.github.com>
2019-04-10 15:55:59 -0300

    Juego V 0.06

---
+++ juego-web/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!---->
+<html lang="es"> <!--lang es un atributo de una etiqueta de html indicamos que va a estar en español el html-->
+    <head> <!--La  cabeza de html se utiliza para declarar cosas atributos o propiedasdes de html-->
+        <meta charset="UTF-8"><!-- meta Esto sirve para dar mas informacion sobre el documento-->
+        <meta name="description" content="Esto es un juego web">
+        <meta name="author" content="Varios">
+
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Juego Web</title><!-- Titulo que va a llevar en la pestaña del navegador -->
+
+    </head>
+    <body> <!--Toda la web esta en el body-->
+        <h1>Bienvenido al canal</h1> <!--Esto es un titular-->
+        <h3>Recordad que el canal</h3>
+        <script src="js/inicio.js"></script>
+    </body>
+</html>
---
+++ juego-web/js/inicio.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() { //Funcion anonima
+    console.log("Javascript funciona");
+}, false); //DOM es DOCUMENT OBJECT MODEL.Agregamos un evento para cuando cargue el documento  Igual que java para escuchar un evento
