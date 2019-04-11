d4865574c9dec62f84589796f059b48a88792380
Alejandro Moya <47389285+Alemoyaa@users.noreply.github.com>
2019-04-10 23:54:11 -0300

    Se le añadieron varias funcionalidades
    Se puede mostrar un cuadrado de distintos colores, nos sirve para entender distintas funciones

--- juego-web/js/inicio.js
+++ juego-web/js/inicio.js
@@ -1,3 +1,16 @@
+// ctrl + f5
+
 document.addEventListener('DOMContentLoaded', function() { //Funcion anonima
-    console.log("Javascript funciona");
-}, false); //DOM es DOCUMENT OBJECT MODEL.Agregamos un evento para cuando cargue el documento  Igual que java para escuchar un evento
+   inicio.iniciarJuego();
+}, false); //DOM es DOCUMENT OBJECT MODEL.Agregamos un evento para cuando cargue el documento . Igual que java para escuchar un evento
+
+var inicio = {
+    iniciarJuego: function(){
+        console.log("Juego iniciado");
+        dimensiones.iniciar();
+		var r = new Rectangulo(0, 0, 100, 100);
+		var r2 = new Rectangulo(100, 0, 100, 100);
+        buclePrincipal.iterar();
+    }
+};
+
