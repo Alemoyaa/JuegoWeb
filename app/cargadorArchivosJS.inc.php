e979af5b9f0cfbf350c6a3c9251c16d9626a50a0
Alejandro Moya <47389285+Alemoyaa@users.noreply.github.com>
2019-04-23 23:16:21 -0300

    Add files via upload

--- app/cargadorArchivosJS.inc.php
+++ app/cargadorArchivosJS.inc.php
@@ -3,21 +3,25 @@
 $fecha = new DateTime();
 
 $fuentesJavascript = array(
-    "js/Punto.js",
-    "js/Sprite.js",
-    "js/ajax.js",
-    "js/teclado.js",
-    "js/Rectangulo.js",
-    "js/rectangulo.js",
-    "js/mando.js",
-    "js/buclePrincipal.js",
-    "js/dimensiones.js",
-    "js/inicio.js",
+	"js/Rectangulo.js",
+	"js/Sprite.js",
+	"js/Tile.js",
+	"js/CapaMapaTiles.js",
+	"js/PaletaSprites.js",
+	"js/listadoEstados.js",
+	"js/ajax.js",
+	"js/EstadoMapamundi.js",
+	"js/maquinaEstados.js",
+	"js/Punto.js",
+	"js/Mapa.js",
+	"js/teclado.js",	
+	"js/mando.js",
+	"js/dimensiones.js",
+	"js/buclePrincipal.js",
+	"js/inicio.js"
 );
 
 foreach($fuentesJavascript as $fuente) {
-    echo'<script src="' . $fuente . '?' . $fecha -> getTimestamp() . '"></script>';
-    echo nl2br("\n");
+	echo '<script src="' . $fuente . '?' . $fecha -> getTimestamp() . '"></script>';
+	echo nl2br("\n");
 }
-
-?>
--- js/buclePrincipal.js
+++ js/buclePrincipal.js
@@ -1,36 +1,36 @@
-//Namespace - Espacio de nombres || solo puede haber una en cada pagina web 
-//mainloop - bucle principal 
-//aps - actualizaciones por segundo || donde esta el enemigo donde el jugador el ordenar hace matematicas para poder ejecutar el juego actualizan 60 veces 
-//fps - frames por segundo || se dibuja 60 veces para la fluidez
+//namespace - espacio de nombres
+//main loop - bucle principal
+//aps - actualizaciones por segundo
+//fps - frames por segundo
 //callback
+//1s = 1000ms
 
 var buclePrincipal = {
-    idEjecucion: null, 
-    ultimoRegistro: 0,
-    aps: 0,
-    fps: 0,
-    iterar: function(registroTemporal) {
-        buclePrincipal.idEjecucion = window.requestAnimationFrame(buclePrincipal.iterar); //Solicitar cuadro de animacion. cada vez que el navegador hace una de sus actualizaciones nos actualizara esa cantidad de veces nuestro juego
-        buclePrincipal.actualizar(registroTemporal);
-        buclePrincipal.dibujar();    
+	idEjecucion: null,
+	ultimoRegistro: 0,
+	aps: 0,
+	fps: 0,
+	iterar: function(registroTemporal) {
+		buclePrincipal.idEjecucion = window.requestAnimationFrame(buclePrincipal.iterar);
+		buclePrincipal.actualizar(registroTemporal);
+		buclePrincipal.dibujar();	
 
-        
-        if(registroTemporal - buclePrincipal.ultimoRegistro > 999) {
-            buclePrincipal.ultimoRegistro = registroTemporal;
-            console.log("APS: " + buclePrincipal.aps + " | FPS: "+buclePrincipal.fps);
-            buclePrincipal.aps=0;
-            buclePrincipal.fps=0;
-        }
-    },
-    detener: function(){
+		if(registroTemporal - buclePrincipal.ultimoRegistro > 999) {
+			buclePrincipal.ultimoRegistro = registroTemporal;
+			console.log("APS: " + buclePrincipal.aps + " | FPS: " + buclePrincipal.fps);
+			buclePrincipal.aps = 0;
+			buclePrincipal.fps = 0;
+		}
+	},
+	detener: function() {
 
-    },
-    actualizar: function(registroTemporal){
-        teclado.reiniciar();
-        mando.actualizar();
-        buclePrincipal.aps++;
-    },
-    dibujar: function(registroTemporal){
-        buclePrincipal.fps++;
-    }
+	},
+	actualizar: function(registroTemporal) {
+		teclado.reiniciar();
+		mando.actualizar();
+		buclePrincipal.aps++;
+	},
+	dibujar: function(registroTemporal) {
+		buclePrincipal.fps++;
+	}
 };
