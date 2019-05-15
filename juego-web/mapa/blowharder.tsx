db7a89a20ee4dad6a557576ad8dc532184fec72d
salinas38m <salinas38m@gmail.com>
2019-05-15 11:31:56 -0300

    Carpeta sobrante borrada

--- juego-web/mapa/blowharder.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="blowharder" tilewidth="16" tileheight="16" tilecount="4408" columns="116">
- <image source="../../../img/blowharder.png" width="1856" height="608"/>
-</tileset>
