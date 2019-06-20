eb09d334840a98c4585fd2fefe246c13958623e7
Alejandro <alejandro5320@gmail.com>
2019-06-20 01:01:54 -0300

    Creacion del tercer mapa
    Creacion de un nuevo mapa, intentando arreglar el segundo

---
+++ conjuntoPatrones/villa48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="villa48" tilewidth="48" tileheight="48" tilecount="1408" columns="64">
+ <image source="../img/villa48.png" width="3072" height="1056"/>
+</tileset>
--- img/villa48.tsx
+++ img/villa48.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="villa48" tilewidth="64" tileheight="64" tilecount="1984" columns="64">
- <image source="villa48.png" width="4096" height="1985"/>
+<tileset version="1.2" tiledversion="1.2.3" name="villa48" tilewidth="64" tileheight="64" tilecount="768" columns="48">
+ <grid orientation="orthogonal" width="48" height="48"/>
+ <image source="villa48.png" width="3072" height="1056"/>
 </tileset>
