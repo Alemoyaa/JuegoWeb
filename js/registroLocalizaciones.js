898b2f20fb50abc74def75279417a3974ec9d3c3
Alejandro <alejandro5320@gmail.com>
2019-06-20 01:09:19 -0300

    Cambios en los registros

--- js/registroLocalizaciones.js
+++ js/registroLocalizaciones.js
@@ -3,7 +3,8 @@ var registroLocalizaciones = {
         let localizaciones = new Array();
         localizaciones.push(new RegistroLocalizacionEntrada("Rockport", "niveles/villa48.json", "img/villa48.nivel.png", 495, 40));
         localizaciones.push(new RegistroLocalizacionEntrada("North Farm", "niveles/NorthFarm.json", "img/northfarm48.nivel.png", 803, 1390));
-        
+        localizaciones.push(new RegistroLocalizacionEntrada("EL bosque", "niveles/bosque48.json", "img/bosque48.nivel.png", 495, 40));
+         
 
         for(var i = 0; i < localizaciones.length; i++) {
             if(nombreLocalizacion == localizaciones[i].nombre) {
